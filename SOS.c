11ba07e456a3ea4e3ab6c2b49b7e24f5876cc4d3
GRxeno <xinipap@gmail.com>
2020-05-05 21:01:30 +0300

    Add files via upload

---
+++ SOS.c
@@ -0,0 +1,337 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+int board[25] = {
+	:,:,:,:,:,
+	:,O,-,S,:,
+	:,S,O,-,:,
+	:,-,-,-,:,
+	:,:,:,:,:,
+}
+*/
+/// 0 , 1 , 2 , 3
+enum { CHARO, CHARS, BORDER, EMPTY };
+enum { HUMANWIN, COMPWIN, DRAW };
+enum { HUMAN, COMP };
+
+int ConvertTo25[9] = {
+	6, 7, 8,
+	11,12,13,
+	16,17,18
+};
+
+void InitialiseBoard(int *board);
+void PrintBoard(int *board);
+int HasEmpty(int *board);
+void MakeMove(int *board, int sq, int side);
+void MakeComputerMove(int *board);
+int minimax(int *board, int depth, int isMax);
+void MakeHumanMove(int *board);
+int check_game(int *board);
+void RunGame();
+
+int main() {
+	srand(time(NULL));
+	RunGame();
+
+	return 0;
+}
+
+// Main Function to run the game
+void RunGame() {
+
+	int GameOver = 0;
+	int Side = COMP;
+	int board[25];
+
+	InitialiseBoard(board);
+	PrintBoard(board);
+
+	while(!GameOver) {
+		if(Side==HUMAN) {
+			MakeHumanMove(board);
+			Side=COMP;
+			PrintBoard(board);
+		} else {
+			MakeComputerMove(board);
+			Side=HUMAN;
+			PrintBoard(board);
+		}
+
+		// if three in a row exists Game is over
+		if (check_game(board) == 1){
+            printf("Game over!\n");
+			GameOver = 1;
+			if(Side==HUMAN) {
+				printf("Computer Wins\n");
+				return;
+			} else {
+				printf("Human Wins\n");
+				return;
+			}
+		}
+
+		// if no more moves, game is a draw
+		if(!HasEmpty(board)) {
+			printf("Game over!\n");
+			GameOver = 1;
+			printf("It's a draw\n");
+			return;
+		}
+	}
+}
+
+// Function to create starting board with O at position 4 or 6
+void InitialiseBoard(int *board) {
+	int index = 0;
+
+	for(index = 0; index < 25; ++index) {
+		board[index] = BORDER;
+	}
+
+	for(index = 0; index < 9; ++index) {
+		board[ConvertTo25[index]] = EMPTY;
+	}
+	int left_right = (rand() % 2);
+	if (left_right == 0){
+        board[ConvertTo25[3]] = CHARO;
+	}
+    else {
+        board[ConvertTo25[5]] = CHARO;
+    }
+}
+
+// Print the 3x3 board
+void PrintBoard(int *board) {
+	int index = 0;
+	char pceChars[] = "OS|-";
+
+	printf("\n\nBoard:\n\n");
+	for(index = 0; index < 9; ++index) {
+		if(index!=0 && index%3==0) {
+			printf("\n\n");
+		}
+		printf("%4c",pceChars[board[ConvertTo25[index]]]);
+	}
+	printf("\n");
+}
+
+// If one EMPTY in board return 1 else 0
+int HasEmpty(int *board) {
+	int index = 0;
+
+	for(index = 0; index < 9; ++index) {
+		if( board[ConvertTo25[index]] == EMPTY) return 1;
+	}
+	return 0;
+}
+
+// Places move to board
+void MakeMove(int *board, int sq, int side) {
+	board[sq] = side;
+}
+
+// Function that makes computer move
+void MakeComputerMove(int *board) {
+    int MoveToMake[2] = {-1, -1};
+    int bestScore = -2147483648;
+    int index;
+    int score;
+
+
+    for (index = 0; index < 9; index++){
+        if (board[ConvertTo25[index]] == EMPTY){
+            MakeMove(board, ConvertTo25[index], CHARO);
+            score = minimax(board, 0, HUMAN);
+            MakeMove(board, ConvertTo25[index], EMPTY);
+            if (score > bestScore){
+                bestScore = score;
+                MoveToMake[0] = index;
+                MoveToMake[1] = CHARO;
+            }
+            MakeMove(board, ConvertTo25[index], CHARS);
+            score = minimax(board, 0, HUMAN);
+            MakeMove(board, ConvertTo25[index], EMPTY);
+            if (score > bestScore){
+                bestScore = score;
+                MoveToMake[0] = index;
+                MoveToMake[1] = CHARS;
+            }
+        }
+    }
+    MoveToMake[0] = ConvertTo25[MoveToMake[0]];
+    MakeMove(board, MoveToMake[0], MoveToMake[1]);
+}
+
+// AI algorithm to find best move
+int minimax(int *board, int depth, int isMax){
+    if (check_game(board) == 1){
+        if (isMax == HUMAN){
+           return 1;
+        }
+        if (isMax == COMP){
+            return -1;
+        }
+    }
+    if(!HasEmpty(board)) {
+        return 0; //tie
+    }
+
+    if (isMax == COMP){
+        int bestScore = -2147483648;
+        int index;
+        int score;
+
+        for (index = 0; index < 9; index++){
+            if (board[ConvertTo25[index]] == EMPTY){
+
+                MakeMove(board, ConvertTo25[index], CHARO);
+                score = minimax(board, depth+1, HUMAN);
+                MakeMove(board, ConvertTo25[index], EMPTY);
+                if (score > bestScore){
+                    bestScore = score;
+                }
+
+                MakeMove(board, ConvertTo25[index], CHARS);
+                score = minimax(board, depth+1, HUMAN);
+                MakeMove(board, ConvertTo25[index], EMPTY);
+                if (score > bestScore){
+                    bestScore = score;
+                }
+            }
+        }
+        return bestScore;
+
+    } else { //(isMax == HUMAN)
+        int bestScore = 2147483647;
+        int index;
+        int score;
+
+        for (index = 0; index < 9; index++){
+            if (board[ConvertTo25[index]] == EMPTY){
+
+                MakeMove(board, ConvertTo25[index], CHARO);
+                score = minimax(board, depth+1, COMP);
+                MakeMove(board, ConvertTo25[index], EMPTY);
+                if (score < bestScore){
+                    bestScore = score;
+                }
+
+                MakeMove(board, ConvertTo25[index], CHARS);
+                score = minimax(board, depth+1, COMP);
+                MakeMove(board, ConvertTo25[index], EMPTY);
+                if (score < bestScore){
+                    bestScore = score;
+                }
+            }
+        }
+        return bestScore;
+
+    }
+}
+
+// Function that takes user input
+void MakeHumanMove(int *board) {
+
+	char userInput[4];
+	int MoveToMake[2] = {-1,-1};
+	int posOk = 0;
+	int charOk = 0;
+	char SO;
+	int move = -1;
+
+	while (posOk == 0) {
+
+		printf("Please enter a move from 1 to 9:");
+		fgets(userInput, 3, stdin);
+		fflush(stdin);
+
+		if(strlen(userInput) != 2) {
+			printf("Invalid strlen()\n");
+			continue;
+		}
+
+		if( sscanf(userInput, "%d", &move) != 1) {
+			move = -1;
+			printf("Invalid sscanf()\n");
+			continue;
+		}
+
+		if( move < 1 || move > 9) {
+			move = -1;
+			printf("Invalid range\n");
+			continue;
+		}
+
+		move--; // Zero indexing
+
+		if( board[ConvertTo25[move]]!=EMPTY) {
+			move=-1;
+			printf("Square not available\n");
+			continue;
+		}
+		posOk = 1;
+	}
+	MoveToMake[0] = ConvertTo25[move];
+
+    while (charOk == 0) {
+
+		printf("Please enter S or O:");
+		fgets(userInput, 3, stdin);
+		fflush(stdin);
+
+		if(strlen(userInput) != 2) {
+			printf("Invalid strlen()\n");
+			continue;
+		}
+
+		if( sscanf(userInput, "%c", &SO) != 1) {
+			printf("Invalid sscanf()\n");
+			continue;
+		}
+
+		if( SO == 'S') {
+            MoveToMake[1] = CHARS;
+            charOk = 1;
+            break;
+		}
+		if( SO == 'O') {
+            MoveToMake[1] = CHARO;
+            charOk = 1;
+            break;
+		}
+		printf("Invalid character\n");
+	}
+
+	printf("Making Move...%d-%c\n",(move+1), SO);
+	//printf("MoveArray: %d,%d\n",moveArray[0], moveArray[1]);
+    MakeMove(board, MoveToMake[0], MoveToMake[1]);
+}
+
+// Check all the possible SOS locations to determine win
+int check_game(int *board){
+
+    int index;
+    for (index = 0; index < 9; index += 3){
+        //printf(" %d %d %d \n",ConvertTo25[index],ConvertTo25[index+1],ConvertTo25[index+2]);
+        if ((board[ConvertTo25[index]] == CHARS) && (board[ConvertTo25[index+1]] == CHARO) &&(board[ConvertTo25[index+2]] == CHARS))
+            return 1;
+    }
+    for (index = 0; index < 3; index++){
+       // printf(" %d %d %d \n",ConvertTo25[index],ConvertTo25[index+3],ConvertTo25[index+6]);
+        if ((board[ConvertTo25[index]] == CHARS) && (board[ConvertTo25[index+3]] == CHARO) &&(board[ConvertTo25[index+6]] == CHARS))
+            return 1;
+    }
+    //printf(" %d %d %d \n",ConvertTo25[0],ConvertTo25[4],ConvertTo25[8]);
+    if ((board[ConvertTo25[0]] == CHARS) && (board[ConvertTo25[4]] == CHARO) &&(board[ConvertTo25[8]] == CHARS))
+            return 1;
+    //printf(" %d %d %d \n",ConvertTo25[2],ConvertTo25[4],ConvertTo25[6]);
+    if ((board[ConvertTo25[2]] == CHARS) && (board[ConvertTo25[4]] == CHARO) &&(board[ConvertTo25[6]] == CHARS))
+            return 1;
+    return -1;
+}
+
+
+
---
+++ ucsv2.c
@@ -0,0 +1,276 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define N                8
+#define n            2*N+1
+#define MAX_INT 2147483647
+#define HASH_TABLE_SIZE 20
+
+typedef struct state {
+    char name[n];
+    int score;
+    int blank;
+    struct state *next;
+    struct state *nextH;
+    struct state *prev;
+} State;
+
+// Global var
+State *root;
+State *hashTable[HASH_TABLE_SIZE];
+
+// Main Functions
+State* ucs();
+void print_best(State *bestState);
+void print_queue();
+void find_next(State *myState);
+void constructRoot(char* buffer);
+int is_gold(State *myState);
+State* change(State *myState, int index);
+// Priority Queue Functions
+void push(State** myState);
+int isEmpty();
+void pop();
+State* peek();
+// HashTable Functions
+int hash(char*name);
+void initializeHashTable();
+void printHashTable();
+int insertInHashTable(State * node);
+State *searchInHashTable(char * name);
+
+int main(){
+    printf("\n---                            UCS                            ---\n");
+    initializeHashTable();
+    char first[n] = "AAAAAAAA-MMMMMMMM";
+    //char first[n] = "AMA-MMA";
+
+    clock_t time_now = clock();
+
+    constructRoot(first);
+    printf(" %s (name)  %d (blank)  %d (score) %d (final)\n", root->name, root->blank, root->score, is_gold(root));
+    insertInHashTable(root);
+
+    State* bestState = malloc(sizeof(State));
+    bestState = ucs();
+
+    if (bestState->score != MAX_INT){
+        print_best(bestState);
+    } else {
+        printf("\n No paths found!\n");
+    }
+
+    time_now = clock() - time_now;
+    double time_taken = ((double)time_now)/CLOCKS_PER_SEC; // in seconds
+    printf("\n -- program took %f seconds to execute -- \n", time_taken);
+}
+
+State* ucs(){
+
+    State* bestState = malloc(sizeof(State));
+    bestState->score = MAX_INT;
+
+    int count_gold = 0;
+    find_next(root);
+
+    while(!isEmpty()){
+
+        State* nowState;
+        nowState = peek();
+        pop();
+
+        if (is_gold(nowState) == 1){
+            count_gold++;
+            if (nowState->score < bestState->score){
+                bestState = nowState;
+                printf(" %s (name)  %d (blank)  %d (score) %d (final)\n", bestState->name, bestState->blank, bestState->score, is_gold(bestState));
+                return bestState;
+            }
+        }
+
+        find_next(nowState);
+    }
+    return bestState;
+}
+
+// Hash the name of the State
+int hash(char*name){
+
+    int hash_value = 0;
+    for(int i = 0;i<n;i++){
+        if(name[i]=='M'){
+            hash_value+=3*i;
+            hash_value=hash_value % HASH_TABLE_SIZE;
+        }
+        else{
+            hash_value+=2*i;
+            hash_value=hash_value % HASH_TABLE_SIZE;
+        }
+    }
+    return hash_value;
+}
+
+// Insert to HashTable
+int insertInHashTable(State * node){
+    if(node == NULL){
+      return 0;
+    }
+    int pos = hash(node->name);
+    node->nextH = hashTable[pos];
+    hashTable[pos] = node;
+    return 1;
+}
+
+// Look in HashTable of name
+State *searchInHashTable(char * name){
+    int pos = hash(name);
+    State * cur = hashTable[pos];
+    while(cur != NULL && strcmp(cur->name,name)!=0){
+        cur = cur->nextH;
+    }
+    return cur;
+}
+
+// Print the HashTable
+void printHashTable(){
+    for(int i = 0;i<HASH_TABLE_SIZE;i++){
+        if(hashTable[i] == NULL){
+          printf("%d : ----\n", i);
+        }
+        else{
+          printf("%d : %s\n",i,hashTable[i]->name);
+        }
+    }
+}
+
+//Initialize the HashTable
+void initializeHashTable(){
+    for(int i = 0;i<HASH_TABLE_SIZE;i++){
+        hashTable[i]=NULL;
+    }
+}
+
+// Function to push according to score
+void push(State** myState) {
+
+    State* start = root;
+
+    // Traverse the list and find a position to insert new node
+    while (start->next != NULL && start->next->score < (*myState)->score) {
+        start = start->next;
+    }
+
+    (*myState)->next = start->next;
+    start->next = (*myState);
+}
+
+// Function to check if list is empty
+int isEmpty() {
+    return root->next == NULL;
+}
+
+// Removes the element with the lowest score form the list
+void pop() {
+    root->next = root->next->next;
+}
+
+// Return the value at head
+State* peek(){
+    return root->next;
+}
+
+// Construct root
+void constructRoot(char* buffer){
+    root = (State*)malloc(sizeof(State));
+    strncpy(root->name, buffer, n);
+    int i;
+    for(i = 0; i < n; i++){
+        if (buffer[i] == '-'){
+                root->blank = i;
+                break;
+        }
+    }
+    root->score = 0;
+    root->next = NULL;
+    root->prev = NULL;
+}
+
+// Function that finds the next states from myState
+void find_next(State *myState){
+
+    int left = myState->blank - N;
+    if (left < 0)
+        left = 0;
+    int right = myState->blank + N;
+    if (right > n-1)
+        right = n-1;
+
+     int i;
+     for (i = left; i < right+1; i++){
+        if (i != myState->blank){
+            State* newState = (State*)malloc(sizeof(State));
+            newState = change(myState, i);
+            if(searchInHashTable(newState->name)==NULL){
+                insertInHashTable(newState);
+                push(&newState);
+            }
+        }
+     }
+}
+
+// Function that swaps the blank space with "index"
+State* change(State *myState, int index){
+    int k = abs(myState->blank - index);
+    State *newState = (State*)malloc(sizeof(State));
+    strncpy(newState->name, myState->name, n);
+    newState->name[index] = '-';
+    newState->name[myState->blank] = myState->name[index];;
+    newState->blank = index;
+    newState->score = myState->score + k;
+    newState->prev = myState;
+    newState->next = NULL;
+    return newState;
+}
+
+// Function that checks if a state is golden
+int is_gold(State *myState){
+    if (myState->name[n-1] != 'A'){
+        return 0;
+    }
+    int i;
+    for (i = 0; i < n-1; i++) {
+        if ((myState->name[i] == 'A')){
+            if (myState->name[i+1] == 'M'){
+                return 0;
+            } else if ((myState->name[i+1] == '-') && (myState->name[i+2] == 'M')) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+// Function that prints the best path
+void print_best(State *bestState){
+    int steps = 0;
+    while (bestState != NULL){
+        steps++;
+        printf("%s <- ", bestState->name);
+        bestState = bestState->prev;
+    }
+
+    printf("Steps: %d\n", steps);
+}
+
+// Function that prints the queue
+void print_queue(){
+    State * start = root;
+    while (start != NULL){
+        printf("%s / ", start->name);
+        start = start->next;
+    }
+    printf("\n");
+}
+
