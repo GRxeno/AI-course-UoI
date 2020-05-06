bb33488d05752f512865af20d7166c6523d06b49
GRxeno <xinipap@gmail.com>
2020-05-06 19:20:53 +0300

    Add files via upload

--- SOS.c
+++ SOS.c
@@ -2,33 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*
-int board[25] = {
-	:,:,:,:,:,
-	:,O,-,S,:,
-	:,S,O,-,:,
-	:,-,-,-,:,
-	:,:,:,:,:,
-}
-*/
-
-enum { CHARO, CHARS, BORDER, EMPTY };
+// Initialize with enum structs my variables for sos game
+enum { CHARO, CHARS, EMPTY };
 enum { HUMAN, COMP };
-
-int ConvertTo25[9] = {
-	6, 7, 8,
-	11,12,13,
-	16,17,18
-};
-
-void InitialiseBoard(int *board);
-void PrintBoard(int *board);
-int HasEmpty(int *board);
-void MakeMove(int *board, int sq, int side);
-void MakeComputerMove(int *board);
-int minimax(int *board, int depth, int isMax);
-void MakeHumanMove(int *board);
-int check_game(int *board);
+// The board
+int board[9];
+// Function prototypes
+void InitialiseBoard();
+void PrintBoard();
+int HasEmpty();
+void MakeMove(int sq, int side);
+void MakeComputerMove();
+int minimax(int depth, int isMax);
+void MakeHumanMove();
+int check_game();
 void RunGame();
 
 int main() {
@@ -43,8 +30,6 @@ void RunGame() {
 
 	int GameOver = 0;
 	int Side = COMP;
-	int board[25];
-
 	InitialiseBoard(board);
 	PrintBoard(board);
 
@@ -83,57 +68,53 @@ void RunGame() {
 }
 
 // Function to create starting board with O at position 4 or 6
-void InitialiseBoard(int *board) {
+void InitialiseBoard() {
 	int index = 0;
 
-	for(index = 0; index < 25; index++) {
-		board[index] = BORDER;
-	}
-
-	for(index = 0; index < 9; index++) {
-		board[ConvertTo25[index]] = EMPTY;
+	for(index = 0; index < 9; ++index) {
+		board[index] = EMPTY;
 	}
 	int left_right = (rand() % 2);
 	if (left_right == 0){
-        board[ConvertTo25[3]] = CHARO;
+        board[3] = CHARO;
 	}
     else {
-        board[ConvertTo25[5]] = CHARO;
+        board[5] = CHARO;
     }
 }
 
-// Print the 3x3 board
-void PrintBoard(int *board) {
+// Print the 9x9 board
+void PrintBoard() {
 	int index = 0;
-	char pceChars[] = "OS|-";
+	char pceChars[] = "OS-";
 
 	printf("\n\nBoard:\n\n");
-	for(index = 0; index < 9; index++) {
+	for(index = 0; index < 9; ++index) {
 		if(index!=0 && index%3==0) {
 			printf("\n\n");
 		}
-		printf("%4c",pceChars[board[ConvertTo25[index]]]);
+		printf("%3c",pceChars[board[index]]);
 	}
 	printf("\n");
 }
 
 // If one EMPTY in board return 1 else 0
-int HasEmpty(int *board) {
+int HasEmpty() {
 	int index = 0;
 
-	for(index = 0; index < 9; index++) {
-		if( board[ConvertTo25[index]] == EMPTY) return 1;
+	for(index = 0; index < 9; ++index) {
+		if( board[index] == EMPTY) return 1; ///CHANGE
 	}
 	return 0;
 }
 
 // Places move to board
-void MakeMove(int *board, int sq, int side) {
+void MakeMove(int sq, int side) {
 	board[sq] = side;
 }
 
 // Function that makes computer move
-void MakeComputerMove(int *board) {
+void MakeComputerMove() {
     int MoveToMake[2] = {-1, -1};
     int bestScore = -2147483648;
     int index;
@@ -141,18 +122,18 @@ void MakeComputerMove(int *board) {
 
 
     for (index = 0; index < 9; index++){
-        if (board[ConvertTo25[index]] == EMPTY){
-            MakeMove(board, ConvertTo25[index], CHARO);
-            score = minimax(board, 0, HUMAN);
-            MakeMove(board, ConvertTo25[index], EMPTY);
+        if (board[index] == EMPTY){
+            MakeMove(index, CHARO);
+            score = minimax(0, HUMAN);
+            MakeMove(index, EMPTY);
             if (score > bestScore){
                 bestScore = score;
                 MoveToMake[0] = index;
                 MoveToMake[1] = CHARO;
             }
-            MakeMove(board, ConvertTo25[index], CHARS);
-            score = minimax(board, 0, HUMAN);
-            MakeMove(board, ConvertTo25[index], EMPTY);
+            MakeMove(index, CHARS);
+            score = minimax(0, HUMAN);
+            MakeMove(index, EMPTY);
             if (score > bestScore){
                 bestScore = score;
                 MoveToMake[0] = index;
@@ -160,12 +141,11 @@ void MakeComputerMove(int *board) {
             }
         }
     }
-    MoveToMake[0] = ConvertTo25[MoveToMake[0]];
-    MakeMove(board, MoveToMake[0], MoveToMake[1]);
+    MakeMove(MoveToMake[0], MoveToMake[1]);
 }
 
 // AI algorithm to find best move
-int minimax(int *board, int depth, int isMax){
+int minimax(int depth, int isMax){
     if (check_game(board) == 1){
         if (isMax == HUMAN){
            return 1;
@@ -184,18 +164,18 @@ int minimax(int *board, int depth, int isMax){
         int score;
 
         for (index = 0; index < 9; index++){
-            if (board[ConvertTo25[index]] == EMPTY){
+            if (board[index] == EMPTY){
 
-                MakeMove(board, ConvertTo25[index], CHARO);
-                score = minimax(board, depth+1, HUMAN);
-                MakeMove(board, ConvertTo25[index], EMPTY);
+                MakeMove(index, CHARO);
+                score = minimax(depth+1, HUMAN);
+                MakeMove(index, EMPTY);
                 if (score > bestScore){
                     bestScore = score;
                 }
 
-                MakeMove(board, ConvertTo25[index], CHARS);
-                score = minimax(board, depth+1, HUMAN);
-                MakeMove(board, ConvertTo25[index], EMPTY);
+                MakeMove(index, CHARS);
+                score = minimax(depth+1, HUMAN);
+                MakeMove(index, EMPTY);
                 if (score > bestScore){
                     bestScore = score;
                 }
@@ -209,18 +189,18 @@ int minimax(int *board, int depth, int isMax){
         int score;
 
         for (index = 0; index < 9; index++){
-            if (board[ConvertTo25[index]] == EMPTY){
+            if (board[index] == EMPTY){
 
-                MakeMove(board, ConvertTo25[index], CHARO);
-                score = minimax(board, depth+1, COMP);
-                MakeMove(board, ConvertTo25[index], EMPTY);
+                MakeMove(index, CHARO);
+                score = minimax(depth+1, COMP);
+                MakeMove(index, EMPTY);
                 if (score < bestScore){
                     bestScore = score;
                 }
 
-                MakeMove(board, ConvertTo25[index], CHARS);
-                score = minimax(board, depth+1, COMP);
-                MakeMove(board, ConvertTo25[index], EMPTY);
+                MakeMove(index, CHARS);
+                score = minimax(depth+1, COMP);
+                MakeMove(index, EMPTY);
                 if (score < bestScore){
                     bestScore = score;
                 }
@@ -232,7 +212,7 @@ int minimax(int *board, int depth, int isMax){
 }
 
 // Function that takes user input
-void MakeHumanMove(int *board) {
+void MakeHumanMove() {
 
 	char userInput[4];
 	int MoveToMake[2] = {-1,-1};
@@ -266,14 +246,14 @@ void MakeHumanMove(int *board) {
 
 		move--; // Zero indexing
 
-		if( board[ConvertTo25[move]]!=EMPTY) {
+		if( board[move]!=EMPTY) {
 			move=-1;
 			printf("Square not available\n");
 			continue;
 		}
 		posOk = 1;
 	}
-	MoveToMake[0] = ConvertTo25[move];
+	MoveToMake[0] = move;
 
     while (charOk == 0) {
 
@@ -306,28 +286,28 @@ void MakeHumanMove(int *board) {
 
 	printf("Making Move...%d-%c\n",(move+1), SO);
 	//printf("MoveArray: %d,%d\n",moveArray[0], moveArray[1]);
-    MakeMove(board, MoveToMake[0], MoveToMake[1]);
+    MakeMove(MoveToMake[0], MoveToMake[1]);
 }
 
 // Check all the possible SOS locations to determine win
-int check_game(int *board){
+int check_game(){
 
     int index;
     for (index = 0; index < 9; index += 3){
         //printf(" %d %d %d \n",ConvertTo25[index],ConvertTo25[index+1],ConvertTo25[index+2]);
-        if ((board[ConvertTo25[index]] == CHARS) && (board[ConvertTo25[index+1]] == CHARO) &&(board[ConvertTo25[index+2]] == CHARS))
+        if ((board[index] == CHARS) && (board[index+1] == CHARO) &&(board[index+2] == CHARS))
             return 1;
     }
     for (index = 0; index < 3; index++){
        // printf(" %d %d %d \n",ConvertTo25[index],ConvertTo25[index+3],ConvertTo25[index+6]);
-        if ((board[ConvertTo25[index]] == CHARS) && (board[ConvertTo25[index+3]] == CHARO) &&(board[ConvertTo25[index+6]] == CHARS))
+        if ((board[index] == CHARS) && (board[index+3] == CHARO) &&(board[index+6] == CHARS))
             return 1;
     }
     //printf(" %d %d %d \n",ConvertTo25[0],ConvertTo25[4],ConvertTo25[8]);
-    if ((board[ConvertTo25[0]] == CHARS) && (board[ConvertTo25[4]] == CHARO) &&(board[ConvertTo25[8]] == CHARS))
+    if ((board[0] == CHARS) && (board[4] == CHARO) &&(board[8] == CHARS))
             return 1;
     //printf(" %d %d %d \n",ConvertTo25[2],ConvertTo25[4],ConvertTo25[6]);
-    if ((board[ConvertTo25[2]] == CHARS) && (board[ConvertTo25[4]] == CHARO) &&(board[ConvertTo25[6]] == CHARS))
+    if ((board[2] == CHARS) && (board[4] == CHARO) &&(board[6] == CHARS))
             return 1;
     return -1;
 }
